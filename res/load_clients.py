7ef2ad79adfe5fea5df5b0a95d7bd890833623d0
sama-gharib <sama.gharib-ali-barura@proton.me>
2025-06-06 16:39:42 +0200

    Add concept proof

---
+++ res/load_clients.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+class Client:
+	def __init__(self, fields):
+		self.name = fields[0]
+		self.surname = fields[1]
+		self.country = fields[2]
+		self.city = fields[3]
+		self.balance = fields[4]
+		self.password = fields[5]
+		self.email = fields[6]
+
+	def insert(self, cur):
+		sql = f"insert into Client values ('{self.name}', '{self.surname}', '{self.country}', '{self.city}', {self.balance}, '{self.password}', '{self.email}')"
+		cur.execute(sql)
+
+if __name__ == '__main__':
+	with sqlite3.connect("clients.db") as con:
+		cur = con.cursor()
+		count = -1
+		with open("clients.csv") as data:
+			for line in data:
+				count += 1
+				if count == 0:
+					continue
+				client = Client(line.split(','))
+				client.insert(cur)
+
+		con.commit()
+		print("Loaded", count, "lines into database !")
---
+++ src/main.rs
@@ -0,0 +1,214 @@
+use std::borrow::BorrowMut;
+use std::io::Write;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+use ollama_rs::Ollama;
+use ollama_rs::generation::completion::request::GenerationRequest;
+
+use sqlite::Value;
+
+macro_rules! prompt_format {
+    () => {
+        "# You\n\
+         You are an expert in relational databases. So much so that\
+         you can only exclusively express yourself in SQL queries.\n\
+         # Database\n\
+         Now, I've got the following single table sqlite3 database : \n\
+            \t`Client(name, surname, country, city, balance, password, email)`\n\
+            \tWhere `email` is the primary key.\n\
+         never use a table or a column that is not in this database, if you're missing\
+         a data, just try and guess it.\n\
+         # What to do\n\
+         Please give me the SQL query to get : {}.\n\
+         # Formatting\n\
+         Add no explanation, comment, introduction, or conclusion.\
+         Never answer more than a single SQL request. If you think you\
+         need multiple requests, just use an union. Remember: a single\
+         request has a single semicolon. This part is VERY important !\
+         your answer will be fed into a program and if it is not well\
+         formated, it can expose very critical security breaches !"
+    };
+}
+
+macro_rules! prompt {
+    ($e: expr) => {
+        format!(prompt_format!(), $e)
+    };
+}
+
+type State<T> = Arc<Mutex<T>>;
+
+#[tokio::main]
+async fn main() {
+    println!(
+        "Welcome to Data Station v0.1 !\nStart prompting or type `/help` for a list of commands."
+    );
+
+    let mut model = "gemma3:latest".to_string();
+
+    loop {
+        print!(">>> ");
+        let _ = std::io::stdout().flush();
+
+        // Reading user input
+        let mut input = String::new();
+        if let Err(_) = std::io::stdin().read_line(&mut input) {
+            println!("An error occured while reading input. Please try again.");
+            continue;
+        }
+        input.pop(); // Removes '\n' from input
+
+        if input.is_empty() {
+            continue;
+        }
+
+        // Checking if user typed a command
+        if input.starts_with('/') {
+            let command: Vec<&str> = input.split(' ').collect();
+            match command[0] {
+                "/exit" => break,
+                "/help" => print_help(),
+                "/model" => match command.get(1) {
+                    Some(arg) => model = arg.to_string(),
+                    None => println!("Current model: {model}"),
+                },
+                _ => println!(
+                    "Unknown command : {}.\nType '/help' for a list of commands.",
+                    command[0]
+                ),
+            }
+        } else {
+            send_prompt(&prompt!(input), &model).await;
+        }
+    }
+}
+
+async fn send_prompt(prompt: &str, model: &str) {
+    let done = State::new(Mutex::new(false));
+
+    // Cycling loading text is printed in parrallel
+    let loading = thread::spawn({
+        let done = Arc::clone(&done);
+        move || {
+            let mut points = 0;
+            loop {
+                print!("\rThinking{}                    ", ".".repeat(points));
+                let _ = std::io::stdout().flush();
+                thread::sleep(Duration::from_millis(300));
+
+                // Checking if main thread is done
+                if **done.lock().unwrap().borrow_mut() {
+                    break;
+                }
+                points = (points + 1) % 4;
+            }
+        }
+    });
+
+    // Waiting for prompt answer
+    let ollama = Ollama::default();
+    let res = ollama
+        .generate(GenerationRequest::new(model.to_string(), prompt))
+        .await;
+
+    // Notifying loading thread that we are done
+    let mut done = done.lock().unwrap();
+    **done.borrow_mut() = true;
+    drop(done);
+
+    let _ = loading.join();
+
+    match res {
+        Ok(res) => {
+            // Executing prompt's response on database
+            let con = sqlite::open("res/clients.db");
+            match con {
+                Err(e) => println!("\rCould not connect to database : {e}"),
+                Ok(con) => {
+                    // Sanitizing prompt's response
+                    let query = res
+                        .response
+                        .split('\n')
+                        .filter(|x| x.len() < 3 || &x[..3] != "```")
+                        .map(|x| x.to_string())
+                        .reduce(|x, y| format!("{x}\n{y}"))
+                        .unwrap();
+                    println!("\r                                \nSending SQL : {query}");
+
+                    // Database execution
+                    let cursor = con.prepare(&query);
+                    match cursor {
+                        Err(e) => println!("Error while executing query : {e}"),
+                        Ok(cursor) => {
+                            // Result handling (just printing for now)
+                            for (i, row) in cursor.into_iter().enumerate() {
+                                match row {
+                                    Err(e) => println!("Error for row {i} : {e}"),
+                                    Ok(row) => {
+                                        // Nice formatting for user's pleasure
+                                        for field in row.iter() {
+                                            print!("{}, ", extract_value(field.1));
+                                        }
+                                        println!();
+                                    }
+                                }
+                            }
+
+                            println!("\rDone !");
+                        }
+                    }
+                }
+            }
+        }
+        Err(e) =>
+        // Sometimes, shit happens
+        {
+            println!("\rFailed to get a response from ollama : {e}")
+        }
+    }
+}
+
+/// Quick parsing function extracting the
+/// text representation of a `sqlite3::Value`
+/// without its debug informations.
+fn extract_value(value: &Value) -> String {
+    // Start of from the debug format
+    let d = format!("{value:?}");
+    let mut result = String::new();
+
+    let mut parenthesis_level = 0;
+
+    // Parsing
+    for c in d.chars() {
+        if c == ')' {
+            parenthesis_level -= 1;
+        }
+        if parenthesis_level >= 1 {
+            result.push(c);
+        }
+        if c == '(' {
+            parenthesis_level += 1;
+        }
+    }
+
+    result
+}
+
+/// Prints the help text which contains
+/// a list of commands and their purpose.
+/// Please don't extract this as a constant
+/// (even though it looks like we should)
+/// because it I plan to make this help
+/// text reactive to available commands.
+fn print_help() {
+    // TODO: Make help reactive
+    println!(
+        "== Data Station v0.1 commands ==\n\
+         /help ....... Displays this text\n\
+         /exit ............ Exits program\n\
+         /model [model] ... Switch model\n\
+            \t No `model` prints current\n"
+    );
+}
