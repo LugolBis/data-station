2c03ef7de329848fd0acc6a9e16b09019d62e8fd
LugolBis <ld.desmares@gmail.com>
2025-06-14 13:39:44 +0200

    Fix the Manager prompt and improve the parsing of the tasks

--- src/agent.rs
+++ src/agent.rs
@@ -1,12 +1,13 @@
 use ollama_rs::Ollama;
 use ollama_rs::generation::completion::request::GenerationRequest;
-use mylog::{error};
+use mylog::{error,info};
 
 use crate::tools::*;
 use crate::utils::*;
 
 pub async fn launch_agent(prompt: &str, model: &str, state_agent: State<String>) {
 
+    let prompt = get_prompt(prompt.to_string(), "Manager");
     let manager_response = Ollama::default()
         .generate(GenerationRequest::new(model.to_string(), prompt))
         .await;
@@ -24,25 +25,42 @@ pub async fn launch_agent(prompt: &str, model: &str, state_agent: State<String>)
     match manager_response {
         Ok(answer) => {
             let answer = answer.response;
-            let tasks = answer.split("---------")
+            info!("Manager response :\n'''\n{}\n'''",answer);
+            let mut tasks = answer.split("---")
                 .into_iter().map(|s| s.to_string()).collect::<Vec<String>>();
+            let _ = tasks.pop(); // Removing the empty task due to the previous .split()
             
             // This will be used to store the result of each previous step.
             let mut answer = String::new();
             let mut step = 2usize;
 
             for task in tasks {
-                let lines = task.split("\n")
-                    .into_iter().collect::<Vec<&str>>();
+                let mut agent_name = String::new();
+                let mut need_previous_step = false;
+                let mut task_prompt = String::new();
 
-                let agent_name = lines[0];
-                let input = lines[1..lines.len()-1].iter()
-                    .map(|s| s.to_string()).collect::<String>();
-                let need_previous_step = lines[lines.len()-1].to_uppercase();
+                if let Ok(task_infos) = parse_task(task) {
+                    agent_name.push_str(&task_infos.0);
+                    need_previous_step = task_infos.1;
+                    task_prompt.push_str(&task_infos.2);
+                }
+                else {
+                    {
+                        let error_msg = format!("Failed when try to parse an intermediate task. The last answer is :\n\n{}",answer);
+                        if let Ok(client_msg) = &mut state_agent.lock() {
+                            client_msg.0 = error_msg;
+                            client_msg.1 = false;
+                        }
+                        else {
+                            error!("Can't get mut the client_msg.")
+                        }
+                    }
+                    return;
+                }
 
-                let mut prompt = get_prompt(input, agent_name);
-                if need_previous_step.contains("YES") && !answer.is_empty() {
-                    prompt.push_str(&format!("**Data that you need**:\n{}",answer));
+                let mut prompt = get_prompt(task_prompt, &agent_name);
+                if need_previous_step && !answer.is_empty() {
+                    prompt.push_str(&format!("[Data that you need]\n{}",answer));
                 }
 
                 let agent_response = Ollama::default()
@@ -51,7 +69,8 @@ pub async fn launch_agent(prompt: &str, model: &str, state_agent: State<String>)
 
                 match agent_response {
                     Ok(agent_answer) => {
-                        match call_tool(agent_name, agent_answer.response).await {
+                        answer.push_str(&agent_answer.response);
+                        match call_tool(&agent_name, agent_answer.response).await {
                             Ok(answer_tool) => {
                                 {
                                     if let Ok(client_msg) = &mut state_agent.lock() {
@@ -67,10 +86,9 @@ pub async fn launch_agent(prompt: &str, model: &str, state_agent: State<String>)
                                 answer.push_str(&answer_tool);
                                 step += 1;
                             },
-                            Err(error_tool) => {
+                            Err(_) => {
                                 {
-                                    let error_msg = format!("ERROR : The {} Agent hasn't completed his task.  |   Step : {}\nFailed to use his tool : {}",
-                                        agent_name, step, error_tool);
+                                    let error_msg = format!("The following answer could be wrong, paid attention :\n{}", answer);
                                     if let Ok(client_msg) = &mut state_agent.lock() {
                                         client_msg.0 = error_msg;
                                         client_msg.1 = false;
@@ -146,9 +164,12 @@ async fn call_tool(agent_name: &str, agent_prompt: String) -> Result<String, Str
                 .map(|s| s.to_string()).collect::<String>();
             action_files(path, action.as_str(), content)
         },
+        "LLM_Core" => {
+            Ok(get_prompt(agent_prompt, "LLM_Core"))
+        },
         _ => {
             error!("Unknow agent : {}",agent_name);
-            Err(format!("The following {} Agent hasn't access to a tool.", agent_name))
+            Err("Catch an unknow Agent, who hasn't access to a tool.".to_string())
         }
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -15,13 +15,37 @@ pub fn get_prompt(input: String, agent_name: &str) -> String {
     }
 }
 
-pub fn unwrap_response(response:  Result<GenerationResponse, OllamaError>) -> String {
-    match response {
-        Ok(answer) => {
-            answer.response
-        },
-        Err(error) => {
-            format!("\rFailed to get a response from ollama : {error}")
+const AGENTS: [&str;3] = ["LLM_Core", "File_System", "Sqlite3"];
+
+fn parse_agent(agent_name: &str) -> Option<String> {
+    for agent in AGENTS {
+        if agent_name.contains(agent) {
+            return Some(agent.to_string())
         }
+    }
+    None
+}
+
+fn parse_need_previous_step(input: &str) -> bool {
+    input.to_uppercase().contains("YES")
+}
+
+pub fn parse_task(task: String) -> Result<(String, bool, String), String> {
+    let lines = task.split("\n")
+        .into_iter().collect::<Vec<&str>>();
+
+    if lines.len() >= 3 {
+        if let Some(agent_name) = parse_agent(lines[0]) {
+            let need_previous_step = parse_need_previous_step(lines[1]);
+            let task_prompt = lines[2..lines.len()].into_iter()
+                .map(|s| s.to_string()).collect::<String>();
+            Ok((agent_name, need_previous_step, task_prompt))
+        }
+        else {
+            Err("Unknow agent name".to_string())
+        }
+    }
+    else {
+        Err("Inconsistant format response from the Manager agent.".to_string())
     }
 }
